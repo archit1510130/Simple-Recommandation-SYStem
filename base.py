182bbdf8dc21922246c8b9950318186fcf72b7f7
archit1510130 <archit.agrawal.003@gmail.com>
2017-08-30 19:30:53 +0530

    modified

--- base.py
+++ base.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+ #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Thu Jul 27 22:17:06 2017
@@ -8,7 +8,6 @@ Created on Thu Jul 27 22:17:06 2017
 
 import pandas as pd
 import numpy as np
-
 
 class popularity_recommender_py():
     def __init__(self):
@@ -26,9 +25,10 @@ class popularity_recommender_py():
         
         train_data_grouped = train_data.groupby([self.item_id]).agg({self.user_id: 'count'}).reset_index()
         train_data_grouped.rename(columns = {'user_id': 'score'},inplace=True)
+        #print(train_data_grouped)
     
         #Sort the songs based upon recommendation score
-        train_data_sort = train_data_grouped.sort_values(['score', self.item_id], ascending = [0,1])
+        train_data_sort = train_data_grouped.sort_values(['score', self.item_id],ascending=[0,1])
     
         #Generate a recommendation rank based upon score
         train_data_sort['Rank'] = train_data_sort['score'].rank(ascending=0, method='first')
@@ -44,8 +44,7 @@ class item_similiarity_recommendation():
         self.user_id=None
         self.item_id=None
         self.cooccurence_matrix=None
-        self.songs_dict=None
-        self.rev_songs_dict=None
+        self.movie_dict=None
         self.item_similarity_recommendations=None
         
     def create(self, train_data, user_id,item_id):   
@@ -55,8 +54,9 @@ class item_similiarity_recommendation():
         
     def get_user_items(self,user):
         user_data=self.train_data[self.train_data[self.user_id]==user]
+        #print(user_data)
         user_items = list(user_data[self.item_id].unique())
-        #print(len(user_items))
+        #print(user_items)
         return user_items
     
     def get_item_user(self,item):
@@ -70,20 +70,20 @@ class item_similiarity_recommendation():
         return all_items
     
     
-    def construct_matrix(self,user_songs,all_songs):
+    def construct_matrix(self,user_movies,all_movies):
         # get users for all songs in user songs
-        user_songs_users=[]
-        for i in user_songs:
-            user_songs_users.append(self.get_item_user(i))
+        user_movies_users=[]
+        for i in user_movies:
+            user_movies_users.append(self.get_item_user(i))
         # initialize the cooccurence matrix
-        cooccurence_matrix=np.matrix(np.zeros(shape=(len(user_songs),len(all_songs))),float)
-        for i in range(len(all_songs)):
-            song_i_data=self.train_data[self.train_data[self.item_id]==all_songs[i]]
-            users_i=set(song_i_data[self.user_id].unique())
+        cooccurence_matrix=np.matrix(np.zeros(shape=(len(user_movies),len(all_movies))),float)
+        for i in range(len(all_movies)):
+            movie_i_data=self.train_data[self.train_data[self.item_id]==all_movies[i]]
+            users_i=set(movie_i_data[self.user_id].unique())
                 
-            for j in range(0,len(user_songs)):
+            for j in range(0,len(user_movies)):
                 # get unique listeners of song j
-                users_j=user_songs_users[j]
+                users_j=user_movies_users[j]
                 # calculate intersection of i and j
                 users_intersection=users_i.intersection(users_j)
                 #print(users_intersection)
@@ -96,33 +96,39 @@ class item_similiarity_recommendation():
         return cooccurence_matrix 
 
 
-    def make_recommadation(self,user,user_songs,all_songs,cooccurence_matrix):
+    def make_recommadation(self,user,user_movies,all_movies,cooccurence_matrix):
         sim_items=cooccurence_matrix.sum(axis=0)/cooccurence_matrix.shape[0]
         sim_items=np.array(sim_items)[0].tolist()
         sort_with_indices=[(e,i) for i ,e in enumerate(list(sim_items))]
         sort_sim_items=sorted(sort_with_indices,reverse=True)
         #print(sort_sim_items)
         
-        col=['user_id','song','score','rank']
+        col=['user_id','movie','score','rank']
         df=pd.DataFrame(columns=col)
         r=1
         re=[]
         le=1
         for i in range(len(sort_sim_items)):
-            if all_songs[sort_sim_items[i][1]] not in user_songs and r<10:
-                df.loc[le]=[user,all_songs[sort_sim_items[i][1]],sort_sim_items[i][0],r]
+            if all_movies[sort_sim_items[i][1]] not in user_movies and r<10:
+                df.loc[le]=[user,all_movies[sort_sim_items[i][1]],sort_sim_items[i][0],r]
                 le=le+1
                 r=r+1
         return df         
                    
                 
     def recommend(self,user):
-        user_songs=self.get_user_items(user)
-        all_songs=self.get_all_items()
-        cooccurence_matrix=self.construct_matrix(user_songs,all_songs)
-        df=self.make_recommadation(user,user_songs,all_songs,cooccurence_matrix)
+        user_movies=self.get_user_items(user)
+        all_movies=self.get_all_items()
+        cooccurence_matrix=self.construct_matrix(user_movies,all_movies)
+        df=self.make_recommadation(user,user_movies,all_movies,cooccurence_matrix)
         return df
+
+
+
+       
         
+    
+    
         
         #print(cooccurence_matrix)
         
--- script.py
+++ script.py
@@ -6,7 +6,7 @@ Created on Tue Jul 25 16:43:46 2017
 @author: andha_coder
 """
 import pandas as pd
-import numpy as np
+import numpy
 u_cols=['user_id','age','sex','occupation','zip_code']
 User_data=pd.read_csv("./data/u.user",sep='|',names=u_cols,encoding='latin-1')
 # now here all the user data in readble format
@@ -23,8 +23,6 @@ df=pd.merge(User_data,rating_data)
 #print(df.columns)
 
 
-
-
 #now items file
 i_cols = ['movie_id', 'movie title' ,'release date','video release date', 'IMDb URL', 'unknown', 'Action', 'Adventure',
  'Animation', 'Children\'s', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Fantasy',
@@ -34,7 +32,7 @@ items = pd.read_csv('./data/u.item', sep='|', names=i_cols,encoding='latin-1')
 
 # now all the three datafrme are merged
 movie_df=pd.merge(df,items)
-
+#print(movie_df)
     
 
 
@@ -49,7 +47,7 @@ movie_grouped=movie_grouped.sort_values(['rating'],ascending=False).head()
 
 
 
-#print(movie_df.columns.tolist())
+print(movie_df.columns.tolist())
 #['user_id', 'age', 'sex', 'occupation', 'zip_code', 'movie_id', 'rating', 'timestamp', 'movie title', 'release date', 
  #'video release date', 'IMDb URL', 'unknown', 'Action', 'Adventure', 'Animation', "Children's", 'Comedy', 'Crime', 'Documentary', 'Drama', 'Fantasy', 'Film-Noir', 'Horror', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'War', 'Western']
 
@@ -60,20 +58,21 @@ users=movie_df['user_id'].unique()
 from sklearn.cross_validation import train_test_split
 import base
 
-
 ## now create a song recommedation sysytem
 train_data, test_data = train_test_split(movie_df, test_size = 0.20, random_state=0)
 # popularity based recommadation sysytem that means movies that are most num of time watched
-user_id=users[15]
+user_id=users[13]
 #train_data_grouped = train_data.groupby(['movie title']).agg({'user_id': 'count'}).reset_index()
 pm=base.popularity_recommender_py()
+
 pm.create(train_data,'user_id','movie title')
+
 #print(pm.popularity_recommendations)
 
 
 # so its the popularity recommendation ....there is no difference b/w any user .
 
-
+##############################################################################
 
 
 # NOW COMES TO THE ITEM SIMILIARITY BASED SYSTEM
@@ -81,13 +80,19 @@ im=base.item_similiarity_recommendation()
 #u=train_data[train_data['user_id']==user_id]
 #print(list(u['movie_id'].unique()))
 #print(train_data.head(2))
-
 im.create(train_data,'user_id','movie title')
-l=im.recommend(user_id)
+l=im.recommend(user_id)# RETURN THE DATAFRAME
+print(l)
+#SO THIS IS THE RECOMMADATION USING THE COOCCURENCE MATRIX.....
 
 
-
-
+##############################################################################
+check=test_data['user_id'].tolist()
+u_id=check[3]
+#print(u_id)
+im.create(train_data,'user_id','movie title')
+l=im.get_user_items(u_id)
+####################################################
 
 
 
